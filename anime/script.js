0709e9270a11366b4071e60935876b538451e436
Nandagopal <ethuku1027@gmail.com>
2021-05-28 17:34:22 +0530

    ADD LOADING

--- anime/script.js
+++ anime/script.js
@@ -2,6 +2,7 @@ const selected = document.querySelector('#category');
 const image = document.querySelector('#img')
 const show = document.querySelector('#btn');
 const errorMsg = document.querySelector('#error');
+const loader = document.querySelector('#loader');
 
 let type = selected.value;
 let link = `https://api.waifu.pics/sfw/waifu`;
@@ -13,19 +14,23 @@ selected.addEventListener('change',()=>{
 
 show.addEventListener('click',displayImage);
 
-function displayImage(){
-    
-    fetch(link,
-        {
-            responseType:'application/json'
-        }
-    )
-    .then((response)=> response.json())
-    .then((response)=> {
-        image.style.display = "block";
-        image.src = `${response.url}`;
-    })
-    .catch((error)=> {
-        errorMsg.textContent = error;
-    });
+
+async function displayImage(){
+    loader.style.display = "block";
+    loader.style.animation = "1s rotation linear infinite";
+    await fetch(link,{
+                mode: 'cors',
+                responseType:'application/json'
+            }
+        )
+        .then((response)=> response.json())
+        .then((response)=> {
+            image.style.display = "block";
+            image.src = `${response.url}`;
+        })
+        .catch((error)=> {
+            errorMsg.textContent = error;
+        });
+    loader.style.display = "none";
+    loader.style.animation = "1s rotation linear";
 }
